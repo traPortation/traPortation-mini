3d9a44eeaa0bddf6c3c2755e3393a81d09cbdb06
reyu0722 <ka15sugar@gmail.com>
2021-01-04 00:00:43 +0900

    人間をprefab化してスタート時に作成

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject person;
+    public int PeopleCount = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatePeople();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreatePeople(){
+        for (int i = 0; i < PeopleCount; i++) {
+            Instantiate(person, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
