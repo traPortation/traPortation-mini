802962fca255d04da84964bdef3bfcb9fd1f7f98
esperanto9657 <esperanto9657@live.jp>
2021-05-05 00:05:25 +0900

    Implement StationManager and the button to build stations

---
+++ Assets/Scripts/StationManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationManager : MonoBehaviour
+{
+    private bool buildMode;
+    private bool buttonClicked;
+    private List<Station> stations;
+    [SerializeField] private GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buttonClicked)
+        {
+            buttonClicked = false;
+        }
+        else if (Input.GetMouseButtonUp(0) && buildMode)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 8f;
+            GameObject newStation = Instantiate(prefab, Camera.main.ScreenToWorldPoint(mousePosition), Quaternion.identity);
+            stations.Add(newStation.GetComponent<Station>());
+        }
+    }
+
+    public void SetBuildMode()
+    {
+        buttonClicked = true;
+        buildMode = !buildMode;
+    }
+}
