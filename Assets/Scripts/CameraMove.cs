d8a5311855956e9a9fc7075fed4ce71c39579bae
AliceSignal <75986941+AliceSignal@users.noreply.github.com>
2021-11-20 17:10:38 +0900

    タッチ操作とマウス操作の同一化及びメインカメラ
    ・タッチ操作とマウス操作を同一の物として扱えるようにしました
    ・メインカメラをドラッグ(スワイプ)操作で動かせるようにしました

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TouchState;
+
+public class CameraMove : MonoBehaviour
+{
+    // 参考：https://qiita.com/shell/items/b8d6bd2c4bd42913189e
+
+    TouchManager touchManager;
+
+    private bool scrollStartFlg = false; // スクロールが始まったかのフラグ
+    private Vector2 scrollStartPos = new Vector2(); // スクロールの起点となるタッチポジション
+
+    private static float SCROLL_DISTANCE_CORRECTION = 0.8f; // スクロール距離の調整
+
+    private Vector2 touchPosition_cam = new Vector2(); // タッチポジション初期化
+    private Collider2D collide2dObj = null; // タッチ位置にあるオブジェクトの判定の初期化
+
+    // Use this for initialization
+    void Start()
+    {
+        this.touchManager = new TouchManager();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.touchManager.update();
+        TouchManager touchState = this.touchManager.getTouch();
+
+        if (touchState.touchFrag)
+        {
+
+            touchPosition_cam = Camera.main.ScreenToWorldPoint(touchState.touchPosition);
+            collide2dObj = Physics2D.OverlapPoint(touchPosition_cam);
+
+            if (scrollStartFlg == false && collide2dObj)
+            {
+                /// <summary>
+                /// タッチ位置にオブジェクトがあったらそのオブジェクトを取得する
+                /// スクロール移動とオブジェクトタッチの処理を区別するために記載
+                /// </summary>
+                GameObject obj = collide2dObj.transform.gameObject;
+                Debug.Log(obj.name);
+            }
+            else
+            {
+                /// <summary>
+                /// タッチした場所に何もない場合、スクロールフラグをtrueに
+                /// </summary>
+                scrollStartFlg = true;
+                if (scrollStartPos.x == 0.0f)
+                {
+                    /// <summary>
+                    /// スクロール開始位置を取得
+                    /// </summary>
+                    scrollStartPos = Camera.main.ScreenToWorldPoint(touchState.touchPosition);
+                }
+                else
+                {
+                    Vector2 touchMovePos = touchPosition_cam;
+                    if (scrollStartPos.x != touchMovePos.x)
+                    {
+                        /// <summary>
+                        /// 直前のタッチ位置との差を取得する
+                        /// </summary>
+                        float diffPos_x = SCROLL_DISTANCE_CORRECTION * (touchMovePos.x - scrollStartPos.x);
+                        float diffPos_y = SCROLL_DISTANCE_CORRECTION * (touchMovePos.y - scrollStartPos.y);
+
+                        Vector3 pos = this.transform.position;
+                        pos.x -= diffPos_x;
+                        pos.y -= diffPos_y;
+
+                        this.transform.position = pos;
+                        scrollStartPos = touchMovePos;
+                    }
+                }
+            }
+        }
+        else
+        {
+
+            /// <summary>
+            /// タッチを離したらフラグを落とし、スクロール開始位置も初期化する 
+            /// </summary>
+            scrollStartFlg = false;
+            scrollStartPos = new Vector2();
+        }
+    }
+}
---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TouchState
+{
+    // 参考：https://techblog.gracetory.co.jp/entry/2018/06/04/000000
+
+
+    /// <summary>
+    /// タッチ操作の参照クラス
+    /// </summary>
+
+    public class TouchManager
+    {
+        public bool touchFrag;      // タッチ有無
+        public Vector2 touchPosition;   // タッチ座標
+        public TouchPhase touchPhase;   // タッチ状態
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="flag"></param>
+        /// <param name="position"></param>
+        /// <param name="phase"></param>
+        public TouchManager(bool flag = false, Vector2? position = null, TouchPhase phase = TouchPhase.Began)
+        {
+            this.touchFrag = flag;
+            if (position == null)
+            {
+                this.touchPosition = new Vector2(0, 0);
+            }
+            else
+            {
+                this.touchPosition = (Vector2)position;
+            }
+            this.touchPhase = phase;
+        }
+
+        // Update is called once per frame
+        public void update()
+        {
+            this.touchFrag = false;
+
+            // マウス操作
+            if (Application.isEditor)
+            {
+                // 押した瞬間
+                if (Input.GetMouseButtonDown(0))
+                {
+                    this.touchFrag = true;
+                    this.touchPhase = TouchPhase.Began;
+                }
+
+                // 離した瞬間
+                if (Input.GetMouseButtonUp(0))
+                {
+                    this.touchFrag = true;
+                    this.touchPhase = TouchPhase.Ended;
+                }
+
+                // 押しっぱなし
+                if (Input.GetMouseButton(0))
+                {
+                    this.touchFrag = true;
+                    this.touchPhase = TouchPhase.Moved;
+                }
+
+                // 座標取得
+                if (this.touchFrag) this.touchPosition = Input.mousePosition;
+            }
+
+            // タッチ操作
+            else
+            {
+                if (Input.touchCount > 0)
+                {
+                    Touch touch = Input.GetTouch(0);
+                    this.touchPosition = touch.position;
+                    this.touchPhase = touch.phase;
+                    this.touchFrag = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// タッチ状態の取得
+        /// </summary>
+        
+        public TouchManager getTouch()
+        {
+            return new TouchManager(this.touchFrag, this.touchPosition, this.touchPhase);
+        }
+    }
+}
