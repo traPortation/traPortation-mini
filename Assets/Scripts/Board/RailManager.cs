14a6f075a6d0c016755cb2d02ab7ba3e8e19bc41
reyu <62667231+reyu0722@users.noreply.github.com>
2022-01-23 23:21:04 +0900

    AddRailがRailを返すように

--- Assets/Scripts/Board/RailManager.cs
+++ Assets/Scripts/Board/RailManager.cs
@@ -17,13 +17,15 @@ public class RailManager : Singleton<RailManager>
         this.rails = new List<Rail>();
     }
 
-    public void AddRail(List<PathNode> edges)
+    public Rail AddRail(List<PathNode> edges)
     {
         int index = this.rails[this.rails.Count-1].ID+1;
         string indexName = index.ToString();
         var rail = new Rail(edges, index, $"Rail {indexName}");
 
         this.rails.Add(rail);
+
+        return rail;
     }
 
 
