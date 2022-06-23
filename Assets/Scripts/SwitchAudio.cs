ba482509ee68348dc3cc62b93cf63c94766d1e14
annin3 <shunta.titech@gmail.com>
2022-06-23 14:16:31 +0900

    命名修正

--- Assets/Scripts/SwitchAudio.cs
+++ Assets/Scripts/SwitchAudio.cs
@@ -5,7 +5,7 @@ public class SwitchAudio : MonoBehaviour {
     private AudioSource[] _audios;
 
     [Range(0, 3)]
-    public int _bgmnum = 0;
+    public int bgmNum = 0;
 
     [Range(0, 1)]
     public float masterVolume = 0.5f;
@@ -19,7 +19,7 @@ public class SwitchAudio : MonoBehaviour {
 
     private void Update () {
         for (int i = 0; i < _audios.Length; i++) {
-            if (i == _bgmnum) {
+            if (i == bgmNum) {
                 _audios[i].volume += 0.01f;
                 if (_audios[i].volume > masterVolume) {
                     _audios[i].volume = masterVolume;
