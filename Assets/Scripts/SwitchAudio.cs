9de1df2c77d0bd56ff558bd341e59f2991e0d874
annin3 <shunta.titech@gmail.com>
2022-06-23 13:53:31 +0900

    SwitchAudioの作成と適用

---
+++ Assets/Scripts/SwitchAudio.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SwitchAudio : MonoBehaviour {
+    [SerializeField]
+    private AudioSource[] _audios;
+
+    [Range(0, 3)]
+    public int _bgmnum = 0;
+
+    [Range(0, 1)]
+    public float masterVolume = 0.5f;
+
+    public void Start() {
+        for (int i = 0; i < _audios.Length; i++) {
+            _audios[i].volume = 0;
+            _audios[i].loop = true;
+        }
+    }
+
+    private void Update () {
+        for (int i = 0; i < _audios.Length; i++) {
+            if (i == _bgmnum) {
+                _audios[i].volume += 0.01f;
+                if (_audios[i].volume > masterVolume) {
+                    _audios[i].volume = masterVolume;
+                }
+            } else {
+                _audios[i].volume -= 0.01f;
+            }
+            
+        }
+        
+    }
+}
