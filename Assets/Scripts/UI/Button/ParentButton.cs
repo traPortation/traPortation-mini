4330171f8101bbd4eade003720a79ed9d8289121
reyu <62667231+reyu0722@users.noreply.github.com>
2022-12-27 21:45:04 +0900

    ボタンが同時に開かないように

--- Assets/Scripts/UI/Button/ParentButton.cs
+++ Assets/Scripts/UI/Button/ParentButton.cs
@@ -7,11 +7,26 @@ public class ParentButton : MonoBehaviour
     [SerializeField]
     private List<GameObject> children;
 
+    [SerializeField]
+    private ParentButton otherButton;
+
+    bool isActive = false;
+
     public void PushParentButton()
     {
+        this.SetActive(!this.isActive);
+        otherButton.SetActive(false);
+    }
+
+    public void SetActive(bool active)
+    {
+        if (this.isActive == active)
+            return;
+
+        this.isActive = active;
         for (int i = 0; i < children.Count; i++)
         {
-            children[i].SetActive(!children[i].activeSelf);
+            children[i].SetActive(active);
         }
     }
 }
