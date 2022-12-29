acff8a056999482d09c487e1ba6711b743b741fa
reyu <62667231+reyu0722@users.noreply.github.com>
2022-12-29 20:47:40 +0900

    解像度の設定を追加

--- Assets/Scripts/MenuSelect.cs
+++ Assets/Scripts/MenuSelect.cs
@@ -1,33 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MenuSelect : MonoBehaviour
 {
-    
-	private AsyncOperation async;
-	[SerializeField]
+
+    private AsyncOperation async;
+    [SerializeField]
     private float fadeInTime;
     [SerializeField]
     private Image image;
 
-    public void StartGame() {
+    void Awake()
+    {
+        Screen.SetResolution(Screen.width, Screen.width * 9 / 16, false);
+    }
+
+    public void StartGame()
+    {
         StartCoroutine("Fadeout");
         fadeInTime = 1f * fadeInTime / 10f;
         SceneManager.LoadScene("Loading");
         Debug.Log("Loading");
-        
-	}
-    public void EndGame() {
+
+    }
+    public void EndGame()
+    {
         Application.Quit();
     }
 
-    IEnumerator Fadeout() {
-        for(var i = 1f; i >= 0; i -= 0.1f) {
+    IEnumerator Fadeout()
+    {
+        for (var i = 1f; i >= 0; i -= 0.1f)
+        {
             var c = image.color;
-			image.color = new Color(c.r, c.g, c.b, i);
-			yield return new WaitForSeconds(0.1f);
-		}
-	}
+            image.color = new Color(c.r, c.g, c.b, i);
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 }
